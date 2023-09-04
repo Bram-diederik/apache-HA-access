c9b386b54c47278e8955db52d902785b7b7dd2c9
Bram Diederik <daft@Blue-mage.lan>
2023-09-04 21:55:43 +0200

    added blacklist to add to normaly open websites added honeypot to automaticly add ip's to blacklist if hit

--- www/youfool.php
+++ www/youfool.php
@@ -1,8 +1,8 @@
 <?php
-//this php script adds the incomming ip address to home assistant to be whitelisted or blacklisted.
+//This php page adds every not whitelisted ip to the blacklist that reaches this page.
 
 $remoteAddr = $_SERVER["REMOTE_ADDR"];
-exec("sudo /usr/local/bin/apache-allow.sh check $remoteAddr"); 
+exec("sudo /usr/local/bin/apache-allow.sh honey $remoteAddr");
 
 header("HTTP/1.1 403 Forbidden");
 header("Status: 403 Forbidden");
@@ -10,5 +10,3 @@ header("Status: 403 Forbidden");
 echo "<h1>403 Forbidden</h1>";
 echo "<p>You don't have permission to access this resource.</p>";
 ?>
-
-
